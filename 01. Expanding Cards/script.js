a060750351fcb5ee31e57be9f7a6441f606e063e
NodirXakimov <nodirxakimov@yandex.ru>
2022-09-30 10:09:11 +0500

    Event KeyCodes added

--- 01. Expanding Cards/script.js
+++ 01. Expanding Cards/script.js
@@ -1,3 +1,18 @@
+// const panels = document.querySelectorAll('.panel')
+// // console.log(panels)
+// panels.forEach((panel) => {
+//     panel.addEventListener('click', () => {
+//         removeActiveClasses()
+//         panel.classList.add('active')
+//     })
+// })
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
+
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach((panel) => {
@@ -6,6 +21,7 @@ panels.forEach((panel) => {
         panel.classList.add('active')
     })
 })
+
 
 function removeActiveClasses() {
     panels.forEach(panel => {
